520f6fd5e964b832a4bcbd5c80de2803d6bbce84
RackReaver <rackreaver@gmail.com>
2020-10-13 13:17:05 -0400

    Added packages field and removed  include_package_data.

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(name='vulcan',
       author='Matt Ferreira',
       author_email='rackreaver@gmail.com',
       url='https://github.com/RackReaver/Vulcan',
-      include_package_data=True,
+      packages=['vulcan'],
       install_requires=install_requirements
       )
