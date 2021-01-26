8555ee7f5a668c03e7cb75ecbfd50ccb95787d27
RackReaver <rackreaver@gmail.com>
2021-01-26 09:56:23 -0500

    feat: :fire: Remove logging function

--- avmp/tools/python_tools.py
+++ avmp/tools/python_tools.py
@@ -3,28 +3,6 @@ import json
 import logging
 
 
-def logging_setup(filename):
-    """Standard logging setup.
-
-    args:
-        filename (str): Script filename.
-    """
-    isinstance(filename, str)
-
-    if os.path.isdir('logs') == False:
-        os.mkdir('logs')
-        logging.info('Creating logs folder.')
-
-    # Logging configuration
-    fmtstr = '%(asctime)s:%(levelname)s:%(module)s:%(message)s'
-    logging.basicConfig(
-        filename='logs/{}.log'.format(filename[:-3]),
-        level=logging.INFO,
-        filemode='a',
-        format=fmtstr
-    )
-
-
 class DataSetup:
     def __init__(self, filename):
         """Standard data file setup.
