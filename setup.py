291ecbf58b307c312c05eb02cbc71f1eec508f1d
RackReaver <rackreaver@gmail.com>
2021-01-26 11:11:59 -0500

    docs: :wrench: Update details in setup file

--- setup.py
+++ setup.py
@@ -1,34 +1,33 @@
-from setuptools import setup, find_namespace_packages
+"""Setup file.
+"""
+__copyright__ = "Copyright (C) 2021  Matt Ferreira"
+__license__ = "Apache License"
 
-# Read the contents of your README file
+from setuptools import setup
+
+# Read contents of README.md file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     README = f.read()
 
-with open(path.join(this_directory, 'vulcan', 'VERSION')) as version_file:
+with open('avmp/core/VERSION') as version_file:
     version = version_file.read().strip()
 assert isinstance(version, str)
 
+install_requirements = []
 
-install_requirements = [
-    # Pre-built object for accessing the Tenable.io API
-    'pyTenable',
-
-    # Pre-built object for accessing the Jira API
-    'jira'
-]
-
-setup(name='vulcan',
-      version=version,
-      description='A collection of tools for managing and automating vulnerability management.',
-      long_description=README,
-      long_description_content_type='text/markdown',
-      license='Apache License',
-      author='Matt Ferreira',
-      author_email='rackreaver@gmail.com',
-      url='https://github.com/RackReaver/Vulcan',
-      download_url='https://github.com/RackReaver/Vulcan',
-      packages=find_namespace_packages(),
-      install_requires=install_requirements
-      )
+setup(name='avmp',
+        version=version,
+        description='Command line vulnerability program manager.',
+        long_description=README,
+        license='Apache License',
+        author='Matt Ferreira',
+        author_email='rackreaver@gmail.com',
+        download_url='https://github.com/RackReaver/AVMP',
+        install_requires=install_requirements,
+        entry_points={
+            'console_scripts': [
+                'avmp=avmp.cli:cli'
+            ]
+        })
