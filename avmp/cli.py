d121bc462e4991de077de3ddc1292c39d6ca6d81
RackReaver <rackreaver@gmail.com>
2021-01-26 11:23:08 -0500

    feat: :construction: Placeholder for command line entry point

---
+++ avmp/cli.py
@@ -0,0 +1,17 @@
+"""Entry point for command line interface.
+"""
+__copyright__ = "Copyright (C) 2021  Matt Ferreira"
+__license__ = "Apache License"
+
+import click
+import json
+
+@click.group()
+@click.option('--config', '-c', default='config.json', show_default=True, help='File location for configurations.')
+def cli(config):
+    """
+    Command line interface used to manage a vulnerability program.
+    """
+    pass
+
+# cli.add_command()
