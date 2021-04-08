7de317a23a5fd0ad5e9a7b60f37ca0b3562398df
RackReaver <rackreaver@gmail.com>
2021-04-08 11:05:57 -0400

    fix: :bug: Rebuilt setup.py file for publishing package

--- setup.py
+++ setup.py
@@ -15,19 +15,16 @@ with open('avmp/core/VERSION') as version_file:
     version = version_file.read().strip()
 assert isinstance(version, str)
 
-install_requirements = []
+install_requirements = ['pyTenable', 'requests']
 
 setup(name='avmp',
-      version=version,
-      description='Command line vulnerability program manager.',
-      long_description=README,
-      license='Apache License',
-      author='Matt Ferreira',
-      author_email='rackreaver@gmail.com',
-      download_url='https://github.com/RackReaver/AVMP',
-      install_requires=install_requirements,
-      entry_points={
-          'console_scripts': [
-              'avmp=avmp.cli.cli:cli'
-          ]
-      })
+    version=version,
+    description='Command line vulnerability program manager.',
+    long_description=README,
+    license='Apache License',
+    author='Matt Ferreira',
+    author_email='rackreaver@gmail.com',
+    download_url='https://github.com/RackReaver/AVMP',
+    install_requires=install_requirements,
+    package=['avmp']
+    })
