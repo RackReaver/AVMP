3e7056f76b56fa061da286da67aa8f139e4b3e6f
RackReaver <rackreaver@gmail.com>
2021-04-15 17:21:33 -0400

    feat: :sparkles: Add ability to log work to seperate ticket if unable to on main ticket

--- avmp/core/updater.py
+++ avmp/core/updater.py
@@ -10,6 +10,7 @@ from avmp.utils.vuln_db import TenableSqliteVulnDB
 
 
 def main(vuln_db_filepath, config):
+    root_ticket = input('Root ticket for logging time: ')
     db = TenableSqliteVulnDB(vuln_db_filepath)
     tickets = db.get_all_tickets()
 
@@ -24,4 +25,14 @@ def main(vuln_db_filepath, config):
     for num, ticket in enumerate(tickets):
         print(f'Updating {ticket[0]} ({num+1} of {len(tickets)})')
         data = jiraAPI._JIRA.issue(ticket[0])
+        logged_work = jiraAPI.log_work(
+            ticket[0], '5m', comment='Updating vulnerability database.')
+
+        # Attempt logging time to root ticket if provided
+        if root_ticket != "" and logged_work == False:
+            root_comment = '{} - {}'.format(
+                ticket[0], COMMENT)
+            jiraAPI.log_work(
+                root_ticket, '5m', comment='Updating vulnerability database.')
+
         db.update_status_by_ticket_number(ticket[0], str(data.fields.status))
