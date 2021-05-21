25002de743e56f21026a0961cfcdca64b2810594
RackReaver <rackreaver@gmail.com>
2021-05-21 14:14:43 -0400

    feat: :construction: Add particle plugin id extractor and scan creator

--- avmp/core/rescan.py
+++ avmp/core/rescan.py
@@ -3,6 +3,7 @@
 __copyright__ = "Copyright (C) 2021  Matt Ferreira"
 __license__ = "Apache License"
 
+import re
 import json
 
 from avmp.core.models import App
@@ -23,13 +24,33 @@ def main(issue, config):
     current = app.jiraAPI._JIRA.issue(issue)
 
     ips = extract_ips(current.fields.description)
+    plugin_id = extract_plugin_id(current.fields.description)
+    input(plugin_id)
 
     # Remove duplicate ips
     ips = [ip for num, ip in enumerate(ips) if ip not in ips[:num]]
 
-    print(app.tenAPI.create_folder('autoRescans - AVMP'))
-    # scan = app.tenAPI.scans.create(name='{} - Rescan'.format(issue),
-    #                                targets=ips)
+    from pprint import pprint
+    pprint(app.tenAPI.tio.plugins.plugin_details(
+        '56209')['attributes'][1]['attribute_value'])
+
+    folder_id = app.tenAPI.create_folder(app.RESCAN_FOLDER_NAME)
+    # scan = app.tenAPI.tio.scans.create(name='{} - Rescan'.format(issue),
+    #                                    folder_id=folder_id,
+    #                                    enabled=True,
+    #                                    launch='ON_DEMAND',
+    #                                    text_targets=','.join(ips))
+
+
+def extract_plugin_id(text):
+    """Given text extract the plugin_id
+
+    args:
+        text (str): String to extract the plugin_id from
+
+    return (list): List of IP Addresses
+    """
+    # TODO: Create plugin id extractor
 
 
 if __name__ == '__main__':
